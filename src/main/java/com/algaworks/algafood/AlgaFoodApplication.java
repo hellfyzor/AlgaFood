fc242c9dc88efd7608b9847ea5d1fc7e48bdd1f6
Márcio Andrade <marcioada@gmail.com>
2021-10-22 16:11:41 -0300

    Começando o projeto e ligação com o banco

---
+++ src/main/java/com/algaworks/algafood/AlgaFoodApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgaFoodApplication.class, args);
+    }
+
+}
